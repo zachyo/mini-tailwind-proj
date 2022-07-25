2216de15af9d5d6b0ce0ea6704c343a22e40f922
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-26 00:58:14 +0100

    second tailwind compilation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Tailwind</h1>
+        <h1 className='text-3xl'>Hello World</h1>
       </header>
     </div>
   );
