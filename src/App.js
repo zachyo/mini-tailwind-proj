1b1ab576d5a774f68d339db2ffe7e9e8dc83f379
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-27 15:04:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Features from './features.component';
+import Hero from './hero.component';
 import Navbar from './nav.component';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Hero/>
+      <Features/>
       <header className="App-header">
         <h1 className='text-5xl'>Hello World</h1>
       </header>
