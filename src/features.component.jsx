7fadd3894f8b101028b58dff5425e154af4c84fa
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-27 15:04:20 +0100

    Create features.component.jsx

---
+++ src/features.component.jsx
@@ -0,0 +1,24 @@
+const Features = () => {
+  return (
+    <div className="container flex flex-col items-center px-6 mx-auto md:flex-row ">
+      <div className="flex flex-col mb-32 space-y-12 md:w-1/2 md:mb-0">
+        <h2 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
+          What's different about Manage?
+        </h2>
+        <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+          laudantium libero odit! Blanditiis magni nihil vitae nostrum odio
+          dicta quos at sequi. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Ipsum modi velit qui aut asperiores accusantium!
+          Necessitatibus earum repellat mollitia vero obcaecati, harum maiores
+          doloribus illum facere perferendis in accusamus eligendi.
+        </p>
+      </div>
+      <div className="flex flex-col justify-start space-y-8 md:w-1/2">
+
+      </div>
+    </div>
+  );
+};
+
+export default Features;
