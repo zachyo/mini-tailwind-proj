cfbd4d7a9b9607340e4694d203cfb1c363c83364
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-28 08:57:50 +0100

    Update features.component.jsx

--- src/features.component.jsx
+++ src/features.component.jsx
@@ -1,6 +1,6 @@
 const Features = () => {
   return (
-    <div className="container flex flex-col items-center px-6 mx-auto md:flex-row ">
+    <div className="container flex flex-col px-6 mx-auto md:flex-row ">
       <div className="flex flex-col mb-32 space-y-12 md:w-1/2 md:mb-0">
         <h2 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
           What's different about Manage?
@@ -10,55 +10,92 @@ const Features = () => {
           laudantium libero odit! Blanditiis magni nihil vitae nostrum odio
           dicta quos at sequi. Lorem ipsum dolor sit amet consectetur
           adipisicing elit. Ipsum modi velit qui aut asperiores accusantium!
-          Necessitatibus earum repellat mollitia vero obcaecati, harum maiores
-          doloribus illum facere perferendis in accusamus eligendi.
+          Necessitatibus earum repellat mollitia vero obcaecati.
         </p>
       </div>
-      <div className="flex flex-col justify-start space-y-8 md:w-1/2">
+      <div className="flex flex-col space-y-8 md:w-1/2">
         {/* List Item */}
-        <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
-          <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
-            <div className="flex items-center space-x-4">
-              <div className="rounded-full text-white py-4 px-6 bg-brightRed md:py-1">
-                01
-              </div>
-              <h3 className="font-bold md:mb-4 pr-9 md:hidden">
-                Track company-wide progress
-              </h3>
-            </div>
-          </div>
-          <h3 className="hidden font-bold text-lg mb-4 md:block ">
-            Track company-wide progress
-          </h3>
-
-          {/* <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent flex items-center space-x-4">
-            <div className="rounded-full text-white py-2 px-4 bg-brightRed md:py-1">
+        <div className="flex flex-col space-y-3 text-left md:items-start md:space-y-0 md:space-x-6 md:flex-row">
+          <div className="rounded-l-full bg-brightRedSupLight flex items-center space-x-4 md:bg-transparent">
+            <div className="rounded-full text-white py-2 px-6 bg-brightRed md:py-1">
               01
             </div>
-            <h3 className="font-bold md:mb-4 pr-9 md:text-base md:hidden">
-              Track company-wide progress
-            </h3>
-            <h3 className="hidden font-bold text-lg mb-4 md:block ">
-              Track company-wide progress
-            </h3>
-          </div> */}
-          {/* <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent flex items-center space-x-4">
-            <div className="rounded-full text-white py-2 px-4 bg-brightRed md:py-1">
-              02
-            </div>
-            <h3 className="font-bold md:mb-4 pr-9 md:text-base">
+            <h3 className="font-bold pr-9 md:hidden ">
               Track company-wide progress
             </h3>
           </div>
-          <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent flex items-center space-x-4">
-            <div className="rounded-full text-white py-2 px-4 bg-brightRed md:py-1">
-              03
-            </div>
-            <h3 className="font-bold md:mb-4 pr-9 md:text-base">
+          <div>
+            <h3 className="hidden font-bold text-lg mb-4 md:block">
               Track company-wide progress
             </h3>
-          </div> */}
+            <p className=" text-darkGrayishBlue">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+              laudantium libero odit! Blanditiis magni nihil vitae nostrum odio
+              dicta quos at sequi. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Ipsum modi velit qui aut asperiores accusantium!
+              Necessitatibus earum repellat mollitia vero obcaecati.
+            </p>
+          </div>
         </div>
+        {/* List Item */}
+        <div className="flex flex-col space-y-3 text-left md:items-start md:space-y-0 md:space-x-6 md:flex-row">
+          <div className="rounded-l-full bg-brightRedSupLight flex items-center space-x-4 md:bg-transparent">
+            <div className="rounded-full text-white py-2 px-6 bg-brightRed md:py-1">
+              02
+            </div>
+            <h3 className="font-bold pr-9 md:hidden ">
+              Everything you need in one place
+            </h3>
+          </div>
+          <div>
+            <h3 className="hidden font-bold text-lg mb-4 md:block">
+              Everything you need in one place
+            </h3>
+            <p className=" text-darkGrayishBlue">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+              consequuntur laudantium assumenda maiores eum quibusdam provident
+              laboriosam velit? Quae, dolorum magnam voluptatem sequi beatae
+              incidunt tempora voluptas harum consequatur esse.
+            </p>
+          </div>
+        </div>
+        {/* List Item */}
+        <div className="flex flex-col space-y-3 text-left md:items-start md:space-y-0 md:space-x-6 md:flex-row">
+          <div className="rounded-l-full bg-brightRedSupLight flex items-center space-x-4 md:bg-transparent">
+            <div className="rounded-full text-white py-2 px-6 bg-brightRed md:py-1">
+              03
+            </div>
+            <h3 className="font-bold pr-9 md:hidden ">
+              Advanced built-in reports
+            </h3>
+          </div>
+          <div>
+            <h3 className="hidden font-bold text-lg mb-4 md:block">
+              Advanced built-in reports
+            </h3>
+            <p className=" text-darkGrayishBlue">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Reprehenderit maxime non asperiores doloremque quasi natus quo
+              similique earum tempora recusandae eligendi cum distinctio harum
+              id, illum totam maiores fugiat. Praesentium.
+            </p>
+          </div>
+        </div>
+        {/* <div className="flex flex-col space-y-4">
+          <div className="rounded-l-full bg-brightRedSupLight flex items-center space-x-4 md:bg-transparent">
+            <div className="rounded-full text-white py-2 px-6 bg-brightRed md:py-1">
+              02
+            </div>
+            <h3 className="font-bold pr-9">Track company-wide progress</h3>
+          </div>
+          <p className="max-w-sm text-darkGrayishBlue ml-0 text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+            laudantium libero odit! Blanditiis magni nihil vitae nostrum odio
+            dicta quos at sequi. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Ipsum modi velit qui aut asperiores accusantium!
+            Necessitatibus earum repellat mollitia vero obcaecati.
+          </p>
+        </div> */}
       </div>
     </div>
   );
