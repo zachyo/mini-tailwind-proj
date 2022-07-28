a522a31ffaaead50aa2dfcbee8b522464368e3e3
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-28 09:07:04 +0100

    Update hero.component.jsx

--- src/hero.component.jsx
+++ src/hero.component.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    // <section id="hero">
+    <section id="hero">
       <div className="container flex flex-col-reverse  items-center px-6 mx-auto mt-10 space-y-0 md:flex-row md:space-y-0">
         <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
           <h1 className="mw-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
@@ -22,7 +22,7 @@ const Hero = () => {
         </div>
         <div className="md:w-1/2"><img src="" alt="illustration" /></div>
       </div>
-    // </section>
+    </section>
   );
 };
 
